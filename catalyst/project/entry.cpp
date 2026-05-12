30fe1767eadef0811474c1c489cc4905edce3360
RedPulseDir <redpulseofficialsup@gmail.com>
2026-05-12 18:24:25 +0400

    Update entry.cpp

--- catalyst/project/entry.cpp
+++ catalyst/project/entry.cpp
@@ -1,31 +1,37 @@
 #include <stdafx.hpp>
-#include <curl/curl.h>
 
 int main( )
 {
     timeBeginPeriod( 1 );
     
-    // Инициализируем curl глобально (один раз)
-    curl_global_init( CURL_GLOBAL_ALL );
-    
     {
         if ( !g::console.initialize( " :> (id recommend you cap your ingame fps for better performance)" ) )
+        {
             return 1;
-            
+        }
+        
         if ( !g::input.initialize( ) )
+        {
             return 1;
-            
+        }
+        
         if ( !g::memory.initialize( L"cs2.exe" ) )
+        {
             return 1;
+        }
     }
     
     {
         if ( !g::modules.initialize( ) )
+        {
             return 1;
-            
-        // Offsets теперь грузятся из интернета
+        }
+        
+        // Offsets теперь грузятся из интернета через WinHTTP
         if ( !g::offsets.initialize( ) )
+        {
             return 1;
+        }
     }
     
     {
@@ -33,9 +39,10 @@ int main( )
         std::thread( threads::combat ).detach( );
         
         if ( !g::render.initialize( ) )
+        {
             return 1;
+        }
     }
     
-    curl_global_cleanup( );
     return 0;
 }
