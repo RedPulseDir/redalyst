d5ee6ef9d680a78bed2604a61d378f0a1ce53ac5
RedPulseDir <redpulseofficialsup@gmail.com>
2026-05-12 18:08:46 +0400

    Update entry.cpp

--- catalyst/project/entry.cpp
+++ catalyst/project/entry.cpp
@@ -1,50 +1,41 @@
 #include <stdafx.hpp>
-
-#include <timeapi.h>
-#pragma comment( lib, "winmm.lib" )
+#include <curl/curl.h>
 
 int main( )
 {
-	timeBeginPeriod( 1 );
-
-	{
-		if ( !g::console.initialize( " :> (id recommend you cap your ingame fps for better performance)" ) )
-		{
-			return 1;
-		}
-
-		if ( !g::input.initialize( ) )
-		{
-			return 1;
-		}
-
-		if ( !g::memory.initialize( L"cs2.exe" ) )
-		{
-			return 1;
-		}
-	}
-
-	{
-		if ( !g::modules.initialize( ) )
-		{
-			return 1;
-		}
-
-		if ( !g::offsets.initialize( ) )
-		{
-			return 1;
-		}
-	}
-
-	{
-		std::thread( threads::game ).detach( );
-		std::thread( threads::combat ).detach( );
-
-		if ( !g::render.initialize( ) )
-		{
-			return 1;
-		}
-	}
-
-	return 0;
+    timeBeginPeriod( 1 );
+    
+    // Инициализируем curl глобально (один раз)
+    curl_global_init( CURL_GLOBAL_ALL );
+    
+    {
+        if ( !g::console.initialize( " :> (id recommend you cap your ingame fps for better performance)" ) )
+            return 1;
+            
+        if ( !g::input.initialize( ) )
+            return 1;
+            
+        if ( !g::memory.initialize( L"cs2.exe" ) )
+            return 1;
+    }
+    
+    {
+        if ( !g::modules.initialize( ) )
+            return 1;
+            
+        // Offsets теперь грузятся из интернета
+        if ( !g::offsets.initialize( ) )
+            return 1;
+    }
+    
+    {
+        std::thread( threads::game ).detach( );
+        std::thread( threads::combat ).detach( );
+        
+        if ( !g::render.initialize( ) )
+            return 1;
+    }
+    
+    curl_global_cleanup( );
+    return 0;
 }
