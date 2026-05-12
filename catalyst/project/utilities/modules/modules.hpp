a1a35d623886a8fe3dff27e2a807ad28def33f39
RedPulseDir <redpulseofficialsup@gmail.com>
2026-05-12 18:10:05 +0400

    Update modules.hpp

--- catalyst/project/utilities/modules/modules.hpp
+++ catalyst/project/utilities/modules/modules.hpp
@@ -1,13 +1,18 @@
+// utilities/modules/modules.hpp
 #pragma once
 
 class modules
 {
 public:
-	bool initialize( );
+    bool initialize( );
+    [[nodiscard]] std::uintptr_t get_base( std::string_view name ) const;
 
-	std::uintptr_t client{ 0 };
-	std::uintptr_t engine2{ 0 };
-	std::uintptr_t tier0{ 0 };
-	std::uintptr_t schemasystem{ 0 };
-	std::uintptr_t vphysics2{ 0 };
+    std::uintptr_t client{ 0 };
+    std::uintptr_t engine2{ 0 };
+    std::uintptr_t tier0{ 0 };
+    std::uintptr_t schemasystem{ 0 };
+    std::uintptr_t vphysics2{ 0 };
+    std::uintptr_t inputsystem{ 0 };
+    std::uintptr_t matchmaking{ 0 };
+    std::uintptr_t soundsystem{ 0 };
 };
